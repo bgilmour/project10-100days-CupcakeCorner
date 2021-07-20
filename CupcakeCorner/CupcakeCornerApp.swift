e3e1010e47276d83b00e5037536b70d2ad421022
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-20 23:39:08 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Bruce Gilmour on 2021-07-20.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
