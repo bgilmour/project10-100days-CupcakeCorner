e9b9d38e98d206db5ea69e398ebe140286662072
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-21 15:16:07 +0100

    Taking basic order details

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,26 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Bruce Gilmour on 2021-07-21.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    @Published var type = 0
+    @Published var quantity = 3
+
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
